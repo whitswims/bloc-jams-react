ab111c2af046c2453ede03aefa3791d4c3309f06
user1 <user1@user1s-MacBook-Air.local>
2018-06-18 14:06:01 -0500

    Checkpoint 7 Audio Controls: completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -12,9 +12,40 @@ class Album extends Component {
     });
 
     this.state = {
-      album: album
+      album: album,
+      currentSong: album.songs[0],
+      isPlaying: false
     };
+
+    this.audioElement = document.createElement('audio');
+    this.audioElement.src = album.songs[0].audioSrc;
   }
+
+  play() {
+    this.audioElement.play();
+    this.setState({ isPlaying: true});
+  }
+
+  pause() {
+    this.audioElement.pause();
+    this.setState({ isPlaying: false });
+  }
+
+  setSong(song) {
+    this.audioElement.src = song.audioSrc;
+    this.setState({ currentSong: song });
+  }
+
+  handleSongClick(song) {
+    const isSameSong = this.state.currentSong === song;
+    if (this.state.isPlaying && isSameSong) {
+      this.pause();
+    } else {
+      if (!isSameSong) { this.setSong(song);}
+      this.play();
+    }
+  }
+
   render() {
     return (
       <section className="album">
@@ -34,11 +65,11 @@ class Album extends Component {
           </colgroup>
           <tbody>
           {
-            this.state.album.songs.map( (songs, index) =>
-              <tr key={index}>
+            this.state.album.songs.map( (song, index) =>
+              <tr className="song" key={index} onClick={() => this.handleSongClick(song)}>
                 <td>{index + 1}. </td>
-                <td>{songs.title}</td>
-                <td>{songs.duration}</td>
+                <td>{song.title}</td>
+                <td>{song.duration}</td>
               </tr>
           )
         }
