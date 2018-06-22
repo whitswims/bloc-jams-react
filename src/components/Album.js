94def00c8b7ce4d9f98c7f31c00611befa0acccf
whitswims <36139554+whitswims@users.noreply.github.com>
2018-06-22 17:24:29 -0500

    Checkpoint 8 Buttons: completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
+import PlayerBar from './PlayerBar';
 
 
 class Album extends Component {
@@ -46,6 +47,15 @@ class Album extends Component {
       this.play();
     }
   }
+
+  handlePrevClick() {
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    const newIndex = Math.max(0, currentIndex -1);
+    const newSong = this.state.album.songs[newIndex];
+    this.setSong(newSong);
+    this.play();
+  }
+
   onMouseEnterHandler(song){
     const isSameSong = this.state.currentSong === song;
       this.setState({hover:true});
@@ -106,6 +116,12 @@ class Album extends Component {
         }
           </tbody>
         </table>
+        <PlayerBar 
+          isPlaying={this.state.isPlaying} 
+          currentSong={this.state.currentSong} 
+          handleSongClick={()=> this.handleSongClick(this.state.currentSong)}
+          handlePrevClick={() => this.handlePrevClick()}
+        />
       </section>
     );
   }
